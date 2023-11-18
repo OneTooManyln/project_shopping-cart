36af0e42f085a84a992b0874359b11ee3169faf5
OneTooMany <santosismael.115@gmail.com>
2023-11-18 13:18:16 -0800

    Create unit tests for slideShow component
    Test: 'renders without errors'
      - Ensures component is rendered without throwing errors.
      - Ensures rendering of all images in array.
        - Uses fakeTimers to allow for next img rendering.

---
+++ src/components/tests/Slideshow.test.jsx
@@ -0,0 +1,32 @@
+import { act, render, screen, fireEvent } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import Slideshow from "../Slideshow";
+
+const images = [
+  { url: "src/assets/181994333_1883185495167039_3378746544883127820_n.jpg" },
+  { url: "src/assets/123434076_1727404487411808_6692900620356230503_n.jpg" },
+  { url: "src/assets/188771268_1898763246942597_110904102917328419_n.jpg" },
+  { url: "src/assets/122959147_1727404594078464_1013389762917053382_n.jpg" },
+  { url: "src/assets/123225339_1727404550745135_6616891483531086405_n.jpg" },
+];
+
+vi.useFakeTimers();
+
+describe("Slideshow Component", () => {
+  it("renders without errors", () => {
+    render(<Slideshow />);
+  });
+
+  it("renders all images in array", async () => {
+    render(<Slideshow />);
+
+    images.forEach((image, index) => {
+      const imageElement = screen.getByAltText(`Slideshow Image ${index + 1}`);
+      expect(imageElement).toBeInTheDocument();
+
+      act(() => {
+        vi.advanceTimersByTime(4000); // Allow time for rendering of next image
+      });
+    });
+  });
+});
