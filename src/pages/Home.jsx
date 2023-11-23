324f97c5d55871c109d2cd2c34373f694226d62f
OneTooMany <santosismael.115@gmail.com>
2023-11-22 16:45:49 -0800

    Enhance Home component styling & layout
    Main content: Define height and padding of main container
    
    Intro content: Use flex to center with parent container.
    
    Text content: Add introduction page text.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <main>
-      <p>HOME</p>
-    </main>
+    <>
+      <main className="px-[20px] h-[400px]">
+        <div className="intro flex justify-center flex-col gap-10 max-w-[1200px] text-[rgb(251,113,153)] text-7xl mt-[5.5%] font-bold px-8 mx-auto">
+          <h2>BLACK FIRDAY SALES IMMINENT</h2>
+          <p className="text-base text-gray-380 text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed felis
+            ex, blandit id pellentesque blandit, commodo tempus purus. Phasellus
+            mollis cursus ante, at ullamcorper augue ultricies ac.
+          </p>
+        </div>
+      </main>
+    </>
   );
 }
