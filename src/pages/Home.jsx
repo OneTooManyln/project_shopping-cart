f99a660bb6efbac5f686077c01417f6359d324d8
OneTooMany <santosismael.115@gmail.com>
2023-11-24 12:24:33 -0800

    Refactor Home component layout
    Main class: Rmv fixed height

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import FeaturedItems from "../components/FeaturedItems";
 export default function Home() {
   return (
     <>
-      <main className="px-[20px] h-[400px]">
+      <main className="px-[20px]">
         <div className="intro flex justify-center flex-col gap-10 max-w-[1200px] text-[rgb(251,113,153)] text-7xl mt-[5.5%] font-bold px-8 mx-auto">
           <h2>BLACK FIRDAY SALES IMMINENT</h2>
           <p className="text-base text-gray-380 text-center">
