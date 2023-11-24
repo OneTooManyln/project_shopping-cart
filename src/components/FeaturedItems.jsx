b38b103afc0eb770e2b75e1ec4c8b45b7bd74677
OneTooMany <santosismael.115@gmail.com>
2023-11-24 12:30:37 -0800

    Update card layout and fixed heights
    Content class: No longer has fixed height of 4000px.
    
    Cards grid layout: Make a 2 X 4 layout with only 5 cards instead of 10.

--- src/components/FeaturedItems.jsx
+++ src/components/FeaturedItems.jsx
@@ -2,41 +2,29 @@ export default function FeaturedItems() {
   return (
     <>
       <div className="featured-items max-w-[1200px] mt-[5.5%] mx-auto">
-        <div className="header flex w-[100%] items-center h-24 bg-[rgb(251,113,153)] order-2 order-black">
+        <div className="header flex w-[100%] items-center h-24 bg-[rgb(251,113,153)]">
           <h3 className="text-3xl h-10 text-center border-b-2 border-white">
             <span className="text pl-4">FEATURED ITEMS</span>
           </h3>
         </div>
-        <div className="content h-[4000px] mt-[5.5%]">
-          <div className="pc-cards h-[1600px] grid grid-rows-3 grid-cols-4 gap-4">
-            <div className="card-wrapper bg-white bg-black">
+        <div className="content mt-[5.5%]">
+          <div className="pc-cards h-[1050px] grid grid-rows-2 grid-cols-4 gap-4">
+            <div className="card-wrapper bg-black">
               <div className="img-container"></div>
             </div>
-            <div className="card-wrapper bg-white bg-black">
+            <div className="card-wrapper bg-black">
               <div className="img-container"></div>
             </div>
-            <div className="card-wrapper bg-white bg-black">
+            <div className="card-wrapper bg-black">
               <div className="img-container"></div>
             </div>
-            <div className="card-wrapper bg-white bg-black">
+            <div className="card-wrapper bg-black">
               <div className="img-container"></div>
             </div>
-            <div className="card-wrapper bg-white bg-black">
+            <div className="card-wrapper bg-black">
               <div className="img-container"></div>
             </div>
-            <div className="card-wrapper bg-white bg-black">
-              <div className="img-container"></div>
-            </div>
-            <div className="card-wrapper bg-white bg-black">
-              <div className="img-container"></div>
-            </div>
-            <div className="card-wrapper bg-white bg-black">
-              <div className="img-container"></div>
-            </div>
-            <div className="card-wrapper bg-white bg-black">
-              <div className="img-container"></div>
-            </div>
-            <div className="card-wrapper bg-white bg-black">
+            <div className="card-wrapper bg-black">
               <div className="img-container"></div>
             </div>
           </div>
