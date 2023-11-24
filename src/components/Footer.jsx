23c3f1eabaa83267700844204ffb6c2b60ac15cb
OneTooMany <santosismael.115@gmail.com>
2023-11-24 15:21:34 -0800

    New component Footer
    Added links: Include placeholder links for social and API used.
    
    Include dummy copyright notice.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="flex flex-col mt-[5.5%] wdith-full h-[160px] bg-[rgb(251,113,153)]">
+        <div className="w-screen border-t-[3px] bg-white border-[rgb(251,113,153)] py-[10px]"></div>
+        <div className="content flex-1 w-[100%] p-8 mx-auto">
+          <div className="links flex gap-2 text-lg">
+            <div className="title">LINKS --</div>
+            <div className="links">
+              <ul className="flex gap-2">
+                <li>twitter</li>
+                <li>github</li>
+                <li>api</li>
+              </ul>
+            </div>
+          </div>
+          <div className="copyright text-sm text-gray-900 mt-[1%]">
+            Copyright @ 2023 All rights reserved.
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
