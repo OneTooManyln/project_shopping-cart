b620c3bd0274ca40aab24a2462010298742a5263
OneTooMany <santosismael.115@gmail.com>
2023-11-25 16:50:54 -0800

    Enhance component error handling and loading states
    Include state variables for error and loading for rending status
    
    Update error state on catch
    
    Update loading state to false when succesfully fethced or encountered error.

--- src/components/UseAPI.jsx
+++ src/components/UseAPI.jsx
@@ -2,22 +2,29 @@ import { useState, useEffect } from "react";
 
 export default function UseAPI() {
   const [APIData, setAPIData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products/category/electronics", {
       mode: "cors",
     })
       .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Server error: ${response.status}`);
+        }
         return response.json();
       })
       .then((data) => {
         setAPIData(data);
-        console.log(APIData);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Something went wrong..", error);
+        setError(error);
+        setLoading(false);
       });
   }, []);
 
-  return APIData;
+  return { APIData, error, loading };
 }
