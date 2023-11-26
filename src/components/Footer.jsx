333513b3f760ae94f00e5fec4507e3585aedf4b4
OneTooMany <santosismael.115@gmail.com>
2023-11-25 16:40:03 -0800

    Improve Footer component positioning
    Give Footer position absolute to be fixed at bottom of page.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="flex flex-col mt-[5.5%] wdith-full h-[160px] bg-[rgb(251,113,153)]">
+      <footer className="flex absolute bottom-0 flex-col mt-[5.5%] width-full -[160px] bg-[rgb(251,113,153)]">
         <div className="w-screen border-t-[3px] bg-white border-[rgb(251,113,153)] py-[10px]"></div>
         <div className="content flex-1 w-[100%] p-8 mx-auto">
           <div className="links flex gap-2 text-lg">
