4cb4b0ff6bf549d1715ba4a49b4fe06279144878
OneTooMany <santosismael.115@gmail.com>
2023-11-28 17:06:01 -0800

    Incorporate API data rendering and fetch handling.
    Handling of API data
      - Display user feedback upon error in fetch or loading
    
    Render Products from API data.

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,24 @@
+import ErrorDisplay from "../components/ErrorDisplay";
+import LoadingDisplay from "../components/LoadingDisplay";
+import RenderProducts from "../components/RenderProducts";
+
 export default function Shop({ APIData, error, loading }) {
+  if (error) {
+    return (
+      <>
+        <ErrorDisplay error={error} />
+      </>
+    );
+  }
+
+  if (loading) {
+    return (
+      <>
+        <LoadingDisplay />
+      </>
+    );
+  }
+
   return (
     <>
       <main className="px-[20px]">
@@ -7,6 +27,9 @@ export default function Shop({ APIData, error, loading }) {
             <h1 className="text-3xl h-10 text-center border-b-2 border-white">
               <span className="shop-text pl-4">Shop</span>
             </h1>
+          </div>
+          <div className="content mt-[5.5%]">
+            <RenderProducts APIData={APIData} />
           </div>
         </div>
       </main>
