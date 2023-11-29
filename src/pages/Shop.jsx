754f458390da86443b3c5768e378759f0552aa2d
OneTooMany <santosismael.115@gmail.com>
2023-11-28 16:37:48 -0800

    Update Shop component with header & container
    Header class: Use flex to layout children & set max width.

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,15 @@
 export default function Shop({ APIData, error, loading }) {
   return (
     <>
-      <h1>SHOP</h1>
+      <main className="px-[20px]">
+        <div className="shop-container max-w-[1200px] mt-[5.5%] mx-auto">
+          <div className="shop-header flex w-[100%] items-center h-24 bg-[rgb(251,113,153)]">
+            <h1 className="text-3xl h-10 text-center border-b-2 border-white">
+              <span className="shop-text pl-4">Shop</span>
+            </h1>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
