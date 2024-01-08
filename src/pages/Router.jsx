1300b373f4b8c29e68ab51349065d1ef65328b9d
OneTooMany <santosismael.115@gmail.com>
2024-01-08 15:44:06 -0800

    Pass prop setProduct as onProduct to Cart component

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -72,6 +72,7 @@ export default function Router() {
             <Cart
               cartItems={cartItems}
               setCartItems={setCartItems}
+              onProduct={setProduct}
               onDelete={deleteItem}
             />
           ),
