bc0310097192599317f411e663bd7fd8ce3af08d
OneTooMany <santosismael.115@gmail.com>
2024-01-09 12:22:54 -0800

    Rmv committed code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,15 +17,7 @@ export default function Header() {
             <div id="box" className="w-20"></div>
           </div>
         </div>
-        <div className="banner-bottom flex justify-center w-screen /*h-[10px]*/ g-[rgb(87,136,206)] bg-white border-b-[1px] border-[rgb(251,113,153)] mt-[3px] py-[10px]">
-          {/* <div className="search-bar flex items-center w-[50%] border-2 rounded border-[rgb(51,51,51)] bg-white h-10 px-[20px] py-[20px]">
-            <img
-              src="/src/assets/search_FILL0_wght400_GRAD0_opsz24.svg"
-              alt="Search bar icon"
-              className="search-bar-icon h-[25px] w-[25px]"
-            />
-          </div> */}
-        </div>
+        <div className="banner-bottom flex justify-center w-screen /*h-[10px]*/ g-[rgb(87,136,206)] bg-white border-b-[1px] border-[rgb(251,113,153)] mt-[3px] py-[10px]"></div>
       </header>
     </>
   );
